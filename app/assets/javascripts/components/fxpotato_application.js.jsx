521664f294c6517e3be0960f46ddceee7c9fdc5e
Liam Stupid Name Humphreys <liam.humphreys1989@gmail.com>
2017-06-04 12:09:07 +0100

    add first react component to index

---
+++ app/assets/javascripts/components/fxpotato_application.js.jsx
@@ -0,0 +1,16 @@
+var FxPotatoApplication = React.createClass({
+  render: function() {
+    return(
+      <div className="container">
+        <div className="jumbotron">
+          <h1>FxPotato demo</h1>
+          <p>by Liam Humphreys</p>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+          </div>
+        </div>
+      </div>
+    )
+  }
+});
