e0901ff700c709a1fa9663548052b831f08de848
Liam Stupid Name Humphreys <liam.humphreys1989@gmail.com>
2017-06-05 11:03:32 +0100

    remove comments

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -1,5 +1,3 @@
 //= require_tree ./components
 
 window.FxPotatoApplication = require('./components/fxpotato_application');
-// window.ConvertForm = require('./components/convert_form');
-// window.ConversionTable = require('./components/conversion_table');
