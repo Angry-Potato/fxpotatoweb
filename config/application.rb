8c44ac49eb1bcdda9131f8f267b4a20c41da2cb3
Liam Stupid Name Humphreys <liam.humphreys1989@gmail.com>
2017-06-05 12:36:48 +0100

    attempt at using babelify so that the code doesn't suffer just because heroku

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Fxpotatoweb
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.browserify_rails.commandline_options = "-t reactify --extension=\".js.jsx\""
+    config.browserify_rails.commandline_options = "-t [ babelify --presets [ es2015 react stage-0 ] --plugins [ syntax-async-functions transform-regenerator ] ] --extension=\".js.jsx\""
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
