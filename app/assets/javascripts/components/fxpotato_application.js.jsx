39d890e1689172cf808ccf3c8a681770b20143a2
Liam Stupid Name Humphreys <liam.humphreys1989@gmail.com>
2017-06-07 12:28:52 +0100

    first pass at new form

--- app/assets/javascripts/components/fxpotato_application.js.jsx
+++ app/assets/javascripts/components/fxpotato_application.js.jsx
@@ -13,7 +13,31 @@ var FxPotatoApplication = React.createClass({
   },
   render: function() {
     return(
-      <Nav />
+      <div>
+        <Nav />
+        <div className="container" >
+          <form className="col s12">
+            <div className="row">
+              <div className="input-field col s12 m6 l3">
+                <input id="base" type="text" className="validate" />
+                  <label for="base">Base currency</label>
+              </div>
+              <div className="input-field col s12 m6 l3">
+                <input id="target" type="text" className="validate" />
+                  <label for="target">Target currency</label>
+              </div>
+              <div className="input-field col s12 m6 l3">
+                <input id="amount" type="number" className="validate" />
+                  <label for="amount">Amount</label>
+              </div>
+              <div className="input-field col s12 m6 l3">
+                <input id="date" type="text" className="datepicker" />
+                  <label for="date">Date</label>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     )
   }
 });
